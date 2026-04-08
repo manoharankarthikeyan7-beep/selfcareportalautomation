ae3702b0aa5095ed2996607a2eea6d1ba5664a1c
Senthil NSenthil <senthiln0281@gmail.com>
2026-04-08 12:01:00 +0000

    Updated index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,14 @@ import { PublicClientApplication } from "@azure/msal-browser";
 import { MsalProvider } from "@azure/msal-react";
 import { msalConfig } from "./authConfig";
 
+// Initialize MSAL
 const msalInstance = new PublicClientApplication(msalConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <React.StrictMode>
     <MsalProvider instance={msalInstance}>
-        <App />
+      <App />
     </MsalProvider>
+  </React.StrictMode>
 );
